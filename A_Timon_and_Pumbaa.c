21d65706e170a8aa4fdfe260a229d7dceab00605
Nasim <mdnasimhosen333@gmail.com>
2025-04-18 15:16:10 +0600

    initialization

---
+++ A_Timon_and_Pumbaa.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int a, b;
+    scanf("%lld %lld", &a, &b);
+    long long int result = a - b;
+    if (result < 0)
+    {
+        result = 0;
+    }
+    // printf("%lld ",result);
+    return 0;
+}
